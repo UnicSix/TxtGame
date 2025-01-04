1f3c551dd0b9b3a4db25e7c594969ce24cc2f1cc
noookia8110 <matchman7980@gmail.com>
2025-01-04 16:19:36 +0800

    fix terrain generate, support map scaling

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,12 @@
 #include <stdexcept>
 #include <utility>
 #include <chrono>
+#include <vector>
 #include "GameTypes.h"
 
-const int mapWidth = 30;
-const int mapHeight = 30;
-const int mapArea = 900;
+const int mapWidth = 60;
+const int mapArea = 1800;
+const int mapHeight = mapArea/mapWidth;
 
 static int score;
 static int treasureCnt=0; // manage amount of treasure in map
@@ -27,18 +28,18 @@ static int treasureCnt=0; // manage amount of treasure in map
 typedef struct graphNode{
   int curPos  = -1;
   int prevPos = -1;
-  int distToStart  = INT32_MAX;
-  int distToDest   = INT32_MAX;
-  int distSum    = INT32_MAX;
-  bool     isVisited = true;
+  int distToStart = INT32_MAX;
+  int distToDest  = INT32_MAX;
+  int distSum     = INT32_MAX;
+  bool isVisited  = true;
   // Value of sum must be updated AFTER distToStart and distToDest
   // There is no possibility of obtaining a negative edge in this case
   bool operator>(const graphNode &other) const{
     return distSum > other.distSum;
   }
   friend std::ostream& operator<<(std::ostream& os, const graphNode&node){
-    os << "Current Node: "  << node.curPos%30 << " ," << node.curPos/30  << "\n"
-       << "Previous Node: " << node.prevPos%30 << " ," << node.prevPos/30 << "\n"
+    os << "Current Node: "  << node.curPos%mapWidth << " ," << node.curPos/mapWidth  << "\n"
+       << "Previous Node: " << node.prevPos%mapWidth << " ," << node.prevPos/mapWidth << "\n"
        << "Sum Dist: "      << node.distSum
        << std::endl;
     return os;
@@ -56,56 +57,38 @@ inline uint32_t currentTopTile(const uint32_t posInfo){
 
 // Notice: the pair parameter here only contain 2 numbers 
 // but represents 2 nodes' coordinates
-void pathfindingAStar(uint32_t *worldMap, graphNode *mapGraph, const std::pair<uint32_t, uint32_t> coord);
+void pathfindingAStar(uint32_t *worldMap, std::vector<graphNode> &mapGraph, const std::pair<uint32_t, uint32_t> coord);
+// void pathfindingAStar(uint32_t *worldMap, graphNode* mapGraph, const std::pair<uint32_t, uint32_t> coord);
 inline int manhattanDist(uint32_t *worldMap, int pA, int pB){
-  return std::abs(pA/30-pB/30) + std::abs(pA%30-pB%30);
+  return std::abs(pA/mapWidth-pB/mapWidth) + std::abs(pA%mapWidth-pB%mapWidth);
 }
 
 int main(){
+  // TODO: recore the path which is actually took by the player
 
   std::cout << "Game Start\n";
   std::srand(std::time(nullptr));
-  uint32_t worldMap[900]={0};
+  uint32_t* worldMap = new uint32_t[mapArea]{0};
 
   int pos;
   int treasurePos;
-  int tarrainSpawnCnt = std::rand()%15+5;
+  int terrainSpawnCnt = std::rand()%35+35;
 
-  for (int i=0; i<20; i++) {
-    std::cout << "Gen Terrain " << i << ' ';
+  for (int i=0; i<terrainSpawnCnt ; i++) {
     genTerrain(worldMap);
   }
   std::cout << std::endl;
   treasurePos = genTreasure(worldMap);
   do {
-    pos = rand()%900;
-  }while (worldMap[pos]&ROCK);
+    pos = rand()%mapArea;
+    std::cout << std::endl << "Choose Position: " << pos << "\n\n";
+  } while (worldMap[pos]&ROCK);
 
-  // edge test case
-  // treasurePos = 899;
-  // pos = 0;
-  // for (int i=0; i<30; i++) {
-  //   worldMap[0] |= PLAYER;
-  //   worldMap[899] |= TREASURE;
-  //   if(i%4==1){
-  //     for (int j=0; j<29; j++) {
-  //       if ((i*30+j)%2==0) {
-  //         worldMap[i*30+j] |= ROCK;
-  //       }
-  //     }
-  //   }
-  //   else if(i%4==3){
-  //     for (int j=1; j<30; j++) {
-  //       worldMap[i*30+j] |= ROCK;
-  //     }
-  //   }
-  // }
-
-  printf("Player Init Location: (%2d, %2d)\n", (pos)%30, (pos)/30);
+  printf("Player Init Location: (%2d, %2d)\n", pos%mapWidth, pos/mapWidth);
   worldMap[pos] |= PLAYER;
   printf("pos: %d, pos val: %d \n", pos, worldMap[pos]);
   std::cout << "'wasd' to move , 'i' for inspect" << std::endl;
-  printMap(worldMap);
+  // printMap(worldMap);
 
   char input;
   uint32_t newPos;
@@ -128,16 +111,19 @@ int main(){
   //   printMap(worldMap);
   // }
   { // block for testing graph functionalities
-    graphNode mapGraph[900];
-    auto start = std::chrono::high_resolution_clock::now();
+    using namespace std::chrono;
+    // graphNode* mapGraph = new graphNode[mapArea];
+    std::vector<graphNode> mapGraph(mapArea);
+    auto start = high_resolution_clock::now();
     pathfindingAStar(worldMap, mapGraph, std::pair<int, int>(pos, treasurePos));
-    auto end = std::chrono::high_resolution_clock::now();
-    auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    auto us = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    
+    std::cout << "Start Pathfinding\n";
+    auto end = high_resolution_clock::now();
+    auto ns = duration_cast<nanoseconds>(end - start);
+    auto us = duration_cast<microseconds>(end - start);
     std::cout << std::fixed << std::setprecision(9)
               << "Execution time: " << ns.count() << " ns"
               << " (" << ns.count() / 1e9 << " seconds)" << std::endl;
+    // delete[](mapGraph);
   }
   printMap(worldMap);
 
@@ -146,10 +132,10 @@ int main(){
 }
 
 void genTerrain(uint32_t *worldMap){
-  int startPos = std::rand()%900;
+  int startPos = std::rand()%mapArea;
   int terrainType = std::rand()%3;
   for(auto offset : terrains.at(terrainType)){
-    if(startPos + offset > 0){ // exclude negative indices
+    if(startPos + offset > 0 && startPos+offset<mapArea){ // exclude negative indices
       // write rock to position
       worldMap[startPos+offset] |= ROCK;
     }
@@ -185,9 +171,9 @@ void genTerrain(uint32_t *worldMap){
 */
 
 uint32_t genTreasure(uint32_t *worldMap){
-  uint32_t treasurePos = std::rand()%900;
+  uint32_t treasurePos = std::rand()%mapArea;
   while((worldMap[treasurePos]&ROCK)){
-    treasurePos = std::rand()%900;
+    treasurePos = std::rand()%mapArea;
   }
   worldMap[treasurePos] |= TREASURE;
   return treasurePos;
@@ -198,19 +184,19 @@ uint32_t playerMove(const uint32_t pos, const char direc, uint32_t *worldMap){
   // Main map bound check
   switch (moveDirec.find(direc)->second) {
     case 0: // Up
-      if (pos<30) {break;}
-      else if(worldMap[pos-30]&ROCK) {break;}
-      else{return pos-30;}
+      if (pos<mapWidth) {break;}
+      else if(worldMap[pos-mapWidth]&ROCK) {break;}
+      else{return pos-mapWidth;}
     case 1: // Down
-      if (pos>=30*29) {break;}
-      else if(worldMap[pos+30]&ROCK){break;}
-      else{return pos+30;}
+      if (pos>=mapWidth*(mapWidth-1)) {break;}
+      else if(worldMap[pos+mapWidth]&ROCK){break;}
+      else{return pos+mapWidth;}
     case 2: // Left
-      if (pos%30==0) {break;}
+      if (pos%mapWidth==0) {break;}
       else if(worldMap[pos-1]&ROCK){break;}
       else{return pos-1;}
     case 3: // Right
-      if (pos%30==29) {break;}
+      if (pos%mapWidth==mapWidth-1) {break;}
       else if(worldMap[pos+1]&ROCK){break;}
       else{return pos+1;}
     default:
@@ -247,16 +233,18 @@ void printMap(const uint32_t *worldMap){
       std::cout << tileChar.find(tile)->second << ' ' << RESET;
       // std::cout << tileChar.find(tile)->second << ' ';
     }
-    if(i%30==29) std::cout << std::endl;
+    if(i%mapWidth==mapWidth-1) std::cout << std::endl;
   }
 }
 
 
-void pathfindingAStar(uint32_t *worldMap, graphNode *mapGraph, const std::pair<uint32_t, uint32_t> coord){
+void pathfindingAStar(uint32_t *worldMap, std::vector<graphNode>& mapGraph, const std::pair<uint32_t, uint32_t> coord){
+// void pathfindingAStar(uint32_t *worldMap, graphNode* mapGraph, const std::pair<uint32_t, uint32_t> coord){
   const uint32_t start = coord.first;
   const uint32_t dest = coord.second;
   uint32_t cur = start;
   uint32_t topPriorityNode = -1;
+  bool *nodeVisited = new bool[mapArea];
   std::priority_queue
     <graphNode, std::vector<graphNode>, std::greater<graphNode>> nodeQueue;
 
@@ -271,44 +259,42 @@ void pathfindingAStar(uint32_t *worldMap, graphNode *mapGraph, const std::pair<u
   // Search for path until visit dest node
   while (true) {
     if(cur == dest){
-      std::cout << "Start node: " << start << std::endl;
-      std::cout << "Current node: " << cur << std::endl;
-      std::cout << "Path is Found" << std::endl;
       break;
     }
   
     // This part needs to be revised if we want to change the ratio of the map
     for(int i=0; i<4; i++){
       int nxt, distSumNxt, distToDestNxt, distToStartNxt;
-      if     (i==0) { nxt = cur>29  ? cur-30 : cur; }
-      else if(i==1) { nxt = cur<870 ? cur+30 : cur; }
-      else if(i==2) { nxt = cur%30!=0 ? cur-1 : cur; }
-      else if(i==3) { nxt = cur%30!=29 ? cur+1 : cur; }
+      if     (i==0) { nxt = cur>mapWidth-1  ? cur-mapWidth : cur; }
+      else if(i==1) { nxt = cur<mapArea-mapWidth? cur+mapWidth : cur; }
+      else if(i==2) { nxt = cur%mapWidth!=0 ? cur-1 : cur; }
+      else if(i==3) { nxt = cur%mapWidth!=mapWidth-1 ? cur+1 : cur; }
       else{ std::cout << "Position Condition Undefined!\n\n"; }
-      if (worldMap[nxt]&ROCK || nxt == mapGraph[cur].prevPos) {
+      if (cur>=mapArea || nxt >= mapArea) {
+        std::cerr << "index out of bound ==";
+      }
+      else if (worldMap[nxt]&ROCK || nxt == mapGraph[cur].prevPos) {
         // Dont walk back to prev node
         // skip every operation, check next node
-        // printf("Skip Node: %2d, %2d\n\n", nxt%30, nxt/30);
+        // printf("Skip Node: %2d, %2d\n\n", nxt%mapWidth, nxt/mapWidth);
+      }
+      else if(worldMap[nxt]&TREASURE){
+        cur = nxt;
+        return;
       }
       else{
-        // printf("Checking Node: %2d, %2d\n\n", nxt%30, nxt/30);
         distToStartNxt = mapGraph[cur].distToStart+1;
         distToDestNxt  = manhattanDist(worldMap, nxt, dest);
         distSumNxt     = distToStartNxt + distToDestNxt;
-        // printf("New Dist Info: %d, %d, %d\n\n", distToStartNxt, distToDestNxt, distSumNxt);
-        // printf("Cur Dist Info: %d, %d, %d\n\n", mapGraph[nxt].distToStart, mapGraph[nxt].distToDest, mapGraph[nxt].distSum);
         // if distSumNxt is less-equal than target node sum, update target node stats
         int shortestInQueue = nodeQueue.empty() ? INT32_MAX : nodeQueue.top().distSum;
-        // printf("Shortest In Queue: %d\n", shortestInQueue);
         if(distSumNxt < mapGraph[nxt].distSum /*&& distSumNxt <= shortestInQueue*/){
-          // printf("Will Push Node: %2d, %2d\n\n", nxt%30, nxt/30);
           mapGraph[nxt].distToStart = distToStartNxt;
           mapGraph[nxt].distToDest  = distToDestNxt;
           mapGraph[nxt].distSum     = distSumNxt;
           mapGraph[nxt].curPos      = nxt;
           mapGraph[nxt].prevPos     = cur;
           mapGraph[nxt].isVisited   = false;
-          // isCheckingQueue = true;
           // if adjacent node available, traverse to it without push it to queue
           if (mapGraph[nxt].distSum == mapGraph[cur].distSum) {
             topPriorityNode = nxt; // next step WILL BE on this position
@@ -316,24 +302,18 @@ void pathfindingAStar(uint32_t *worldMap, graphNode *mapGraph, const std::pair<u
         }
         // pushing node into the queue
         if (mapGraph[nxt].distSum != INT32_MAX && !mapGraph[nxt].isVisited && nxt!=topPriorityNode) {
-          // std::cout << "Push node: " << nxt << "\n";
-          // printf("Push Node:   %2d, %2d\n", nxt%30, nxt/30);
-          // printf("Node Dist:   %d\n\n", mapGraph[nxt].distSum);
           mapGraph[nxt].isVisited = true;
           nodeQueue.push(mapGraph[nxt]);
         }
       }
     } // end of for-loop
-    if (topPriorityNode!=-1) {
+    if (topPriorityNode!=-1 && topPriorityNode<mapArea) {
       cur = topPriorityNode;
       mapGraph[cur].isVisited = true;
-      // printf("Fast Visit Node:  %2d, %2d\n", cur%30, cur/30);
-      // printf("Node Dist :  %d\n\n", mapGraph[cur].distSum);
       topPriorityNode=-1;
     }
     else if(!nodeQueue.empty()){
       cur = nodeQueue.top().curPos;
-      // std::cout << mapGraph[cur] << std::endl;
       nodeQueue.pop();
     }
     else if (nodeQueue.empty()) {
@@ -343,7 +323,13 @@ void pathfindingAStar(uint32_t *worldMap, graphNode *mapGraph, const std::pair<u
     worldMap[cur] |= PATH;
     // printMap(worldMap);
   }
+  std::cout << "Start node: " << start << std::endl;
+  std::cout << "Current node: " << cur << std::endl;
+  std::cout << "Path is Found" << std::endl;
   std::cout << "End pathfinding \n";
+  delete[](nodeVisited);
+
+ return;
 }
 
 
